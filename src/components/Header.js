07f189a766d050716419282aee359c3a9c67ba7b
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-13 16:49:48 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Title from "./Title.js"
+
+export default function Header() {
+  return (
+    <div>
+      <Title text="Jesper Lindqvist" />
+    </div>
+  )
+}
---
+++ src/components/Title.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function Title({ text }) {
+  return <h3 className="text-center text-2xl md:text-4xl uppercase">{text}</h3>
+}
