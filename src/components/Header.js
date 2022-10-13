d648b7fb125ae83e504c275cb918841aba87e53e
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-13 17:44:24 +0200

    worked on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,15 @@ import Title from "./Title.js"
 
 export default function Header() {
   return (
-    <div>
-      <Title text="Jesper Lindqvist" />
+    <div className="flex flex-col justify-between h-screen">
+      <div>
+        <p className="text-center text-xl">Jesper Lindqvist</p>
+      </div>
+      <div className="my-auto">
+        <Title text="Hi there" />
+        <Title text="Nice to meet you" />
+        <Title text="Enjoy your stay" />
+      </div>
     </div>
   )
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Title({ text }) {
-  return <h3 className="text-center text-2xl md:text-4xl uppercase">{text}</h3>
+  return <h3 className="text-center text-4xl md:text-6xl uppercase">{text}</h3>
 }
