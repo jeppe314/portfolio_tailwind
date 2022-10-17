98a7815736594e074505e3d1139e5b2f54fa82c3
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-17 22:03:08 +0200

    Worked on spinning tech stack circle

---
+++ src/components/StackCircle.js
@@ -0,0 +1,22 @@
+import React, { useRef, createRef, useEffect } from "react"
+import Stack from "../assets/Stack.json"
+import StackItem from "./StackItem"
+
+export default function StackCircle({}) {
+  const stackIcons = Stack.data.map((stackItem) => (
+    <StackItem url={`${stackItem.url}`} name={stackItem.name} className="absolute top-50" />
+  ))
+
+  return (
+    <div className="flex justify-center items-center h-screen w-screen bg-black">
+      <div className="w-64 h-64 relative animate-spinSlow rounded-full border-2 border-dashed ">
+        <StackItem
+          url="/images/tech_stack/react.svg"
+          position="transform origin-left rotate-[270deg] translate-x-full translate-y-full"
+        />
+
+        {/* <StackItem url="/images/tech_stack/react.svg" top="top-32" /> */}
+      </div>
+    </div>
+  )
+}
